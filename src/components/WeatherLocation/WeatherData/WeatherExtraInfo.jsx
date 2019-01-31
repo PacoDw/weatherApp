495f5342ad8b55255fc78cdb6346005b92af4e64
Paco Preciado Mendoza <__pm@outlook.com>
2019-01-31 12:38:11 -0600

    :fire: humidity text was modifed

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.jsx
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const WeatherExtraInfo = ({humidity, wind}) => {
   return (
     <div className='WeatherExtraInfoCont'>
-        <span className='extraInfoText'>{`humidity ${humidity}%  -  `}</span>
+        <span className='extraInfoText'>{`humidity ${humidity}%`}</span>
         <span className='extraInfoText'>{`wind ${wind} m/s`}</span>
     </div>
   )

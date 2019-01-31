53100b84d9ce56844e5d84d4cc176e7e3f2b2884
Paco Preciado Mendoza <__pm@outlook.com>
2019-01-31 12:35:27 -0600

    :sparkles: react-flexbox-grid was applied and localList was added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import WeatherLocation from './components/WeatherLocation'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+import { Grid, Col, Row } from 'react-flexbox-grid';
+import LocationList from './components/LocationList'
 import './App.css';
+
+const cities = [
+  "Manzanillo,mx",
+  "Colima,mx",
+  "Guadalajara,mx",
+  "Washington,us",
+  "New York,us",
+  "Chicago,us",
+  "Madrid,es",
+  "Buenos Aires,ar"
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <WeatherLocation city={'Guadalajara,mx'}/>
-      </div>
+      <Grid>
+        <AppBar position="sticky">
+          <Toolbar>
+            <Typography variant='title' color='inherit'>
+              Weather App
+          </Typography>
+          </Toolbar>
+        </AppBar>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList cities={cities} />
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation='4'>
+              <div className="details"></div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }

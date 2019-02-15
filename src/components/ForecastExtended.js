4ae847b336b929aef646607767622becbdd1f64c
Paco Preciado Mendoza <__pm@outlook.com>
2019-02-15 11:48:21 -0600

    :sparkles: ForecastExtended was added

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,15 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ForecastItem from './ForecastItem'
+
+import getUrlWeatherByCity from '../services/getUrlWeatherByCity';
+import transformForecast from '../services/transformForecast';
 
 import "./style.css"
+
 class ForecastExtended extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      forecastData: null,
+    }
+  }
 
+  componentDidMount() {
+    const url_forecast = getUrlWeatherByCity('forecast', this.props.city);
+    fetch(url_forecast)
+      .then(res => res.json())
+      .then(data => this.setState({ forecastData: transformForecast(data) }))
+  }
 
-  render () {
-    const {city} = this.props
+  renderForecastItemDays() {
+    return this.state.forecastData.map(forecast => (
+      <ForecastItem
+        weekDay={forecast.weekDay}
+        hour={forecast.hour}
+        data={forecast.data}
+      />
+    ))
+  }
+
+  renderProgress = () => {
+    return <h3>Loading Extended Pronostic...</h3>
+  }
+
+  render() {
+    const { city } = this.props
+    const { forecastData } = this.state
     return (
       <div>
         <h2 className='forecast-title'>Enxtended Pronostic to {city}</h2>
+        {forecastData
+          ? this.renderForecastItemDays()
+          : this.renderProgress()
+        }
       </div>
     )
   }

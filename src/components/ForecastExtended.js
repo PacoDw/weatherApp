be77bf0a072f940913a2a135c884d7fb19e12ee8
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-03 13:42:10 -0600

    :fire: Refactoring code, chenge class to function component

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -2,69 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ForecastItem from './ForecastItem'
 
-import getUrlWeatherByCity from '../services/getUrlWeatherByCity';
-import transformForecast from '../services/transformForecast';
-
 import "./style.css"
 
-class ForecastExtended extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      forecastData: null,
-    } 
-  }
-
-  componentDidMount() {
-    this.updateCity(this.props.city)
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.city !== this.props.city) {
-      this.setState({forecastData:null})   // This allow us to show the loading state
-      this.updateCity(this.props.city)      // When the promise is resolve, it show us the data
-    }
-  }
-
-  updateCity = city => {
-    const url_forecast = getUrlWeatherByCity('forecast', city);
-    fetch(url_forecast)
-      .then(res => res.json())
-      .then(data => this.setState({ forecastData: transformForecast(data) }) )
-  }
-
-  renderForecastItemDays() {
-    return this.state.forecastData.map(forecast => (
-      <ForecastItem
-        key={`${forecast.weekDay}${forecast.hour}`}
-        weekDay={forecast.weekDay}
-        hour={forecast.hour}
-        data={forecast.data}
-      />
-    ))
-  }
-
-  renderProgress = () => {
-    return <h3>Loading Extended Pronostic...</h3>
-  }
-
-  render() {
-    const { city } = this.props
-    const { forecastData } = this.state
-    return (
-      <div>
-        <h2 className='forecast-title'>Enxtended Pronostic to {city}</h2>
-        {forecastData
-          ? this.renderForecastItemDays()
-          : this.renderProgress()
-        }
-      </div>
-    )
-  }
+const renderForecastItemDays = forecastData => {
+  return forecastData.map(forecast => (
+    <ForecastItem
+      key={`${forecast.weekDay}${forecast.hour}`}
+      weekDay={forecast.weekDay}
+      hour={forecast.hour}
+      data={forecast.data}
+    />
+  ))
 }
+
+const renderProgress = () => {
+  return <h3>Loading Extended Pronostic...</h3>
+}
+
+const ForecastExtended = ({ city, forecastData }) => (
+    <div>
+      <h2 className='forecast-title'>Enxtended Pronostic to {city}</h2>
+      {forecastData
+        ? renderForecastItemDays(forecastData)
+        : renderProgress()
+      }
+    </div>
+)
 
 ForecastExtended.propTypes = {
   city: PropTypes.string.isRequired,
+  forecastData: PropTypes.array.isRequired,
 }
 
 export default ForecastExtended
@@ -100,4 +67,11 @@ export default ForecastExtended
   //     return {city: nextProps.city}
   //   }
   //   else return null
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.city !== this.props.city) {
+  //     this.setState({forecastData:null})   // This allow us to show the loading state
+  //     this.updateCity(this.props.city)      // When the promise is resolve, it show us the data
+  //   }
   // }

2a93f238a2683edc4877114480e555a2ac6923ab
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-03 12:14:14 -0600

    :fire: setSelectedCity was added and refacotring code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,20 @@
-export const SET_CITY = 'SET_CITY' 
+import getUrlWeatherByCity from './../services/getUrlWeatherByCity'
+import transformForecast from './../services/transformForecast'
 
-// Accion Creator
-export const setCity = payload => ( { type: SET_CITY, payload } )
+export const SET_CITY = 'SET_CITY' 
+export const SET_FORECASTDATA = 'SET_FORECASTDATA'
+
+/* Accions Creator 
+*/
+const setCity = payload => ( { type: SET_CITY, payload } )
+const setForcastData = payload => ( { type: SET_FORECASTDATA,  payload } )
+
+export const setSelectedCity = payload => {
+  return dispatch => {
+    const url_forecast = getUrlWeatherByCity('forecast', payload);
+    dispatch(setCity(payload))
+    fetch(url_forecast)
+      .then(res => res.json())
+      .then(data => dispatch(setForcastData({ city: payload, forecastData: transformForecast(data) })) )
+  }
+}

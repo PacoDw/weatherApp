0a9e5fdcd84a8dcb0f20a9813761e500779bd1ab
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-04 17:08:41 -0600

    :sparkles: getWeatherCities and refactoring code was implementing

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -1,19 +1,20 @@
-import { SET_FORECASTDATA, CLEAR_WEATHER_CITY, SET_WEATHER_CITY  } from './../actions'
+import { SET_FORECASTDATA, CLEAR_WEATHER_CITY, SET_WEATHER_CITY } from './../actions'
 import { createSelector } from 'reselect'
+import toPairs from 'lodash.topairs'
 
 export const cities = (state = {}, action) => {
   switch (action.type) {
     case SET_FORECASTDATA: {
       const { city, forecastData } = action.payload
-      return { ...state, [city]: { forecastData } }
+      return { ...state, [city]: { ...state[city], forecastData } }
     }
     case CLEAR_WEATHER_CITY: {
       const city = action.payload
-      return { ...state,  [city]: { weatherData: null } }
+      return { ...state, [city]: { ...state[city], weatherData: null } }
     }
     case SET_WEATHER_CITY: {
       const { city, weatherData } = action.payload
-      return { ...state,  [city]: { weatherData } }
+      return { ...state, [city]: { ...state[city], weatherData } }
     }
     default:
       return state
@@ -21,6 +22,19 @@ export const cities = (state = {}, action) => {
 }
 
 export const getForcastDataFromCities = createSelector(
-  [(state, city) => cities[city] && cities[city].forecastData],
+  [(state, city) => state[city] && state[city].forecastData],
   forecastData => forecastData
+)
+
+const fromObjToArray = cities => (
+  toPairs(cities).map(([key, value]) => ({
+    key,
+    name: key,
+    data: value.weatherData
+  }))
+)
+
+export const getWeatherCities = createSelector(
+  state => fromObjToArray(state),
+  cities => cities
 )

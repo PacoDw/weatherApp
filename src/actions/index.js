1c94c8287152420c5761b30773d4174ded4016b6
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-04 14:39:58 -0600

    :fire: setWeatherCity, clearWeatherCity and setWeather actions were created

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,19 @@
 import getUrlWeatherByCity from './../services/getUrlWeatherByCity'
 import transformForecast from './../services/transformForecast'
+import transformWeather from '../services/transformWeather';
 
 export const SET_CITY = 'SET_CITY' 
 export const SET_FORECASTDATA = 'SET_FORECASTDATA'
+export const CLEAR_WEATHER_CITY = 'CLEAR_WEATHER_CITY'
+export const SET_WEATHER_CITY = 'SET_WEATHER_CITY'
 
 /* Accions Creator 
 */
 const setCity = payload => ( { type: SET_CITY, payload } )
 const setForcastData = payload => ( { type: SET_FORECASTDATA,  payload } )
+
+const setWeatherCity = payload => ( { type: SET_WEATHER_CITY, payload } )
+const clearWeatherCity = payload => ( { type: CLEAR_WEATHER_CITY, payload } )
 
 export const setSelectedCity = payload => {
   return dispatch => {
@@ -16,5 +22,20 @@ export const setSelectedCity = payload => {
     fetch(url_forecast)
       .then(res => res.json())
       .then(data => dispatch(setForcastData({ city: payload, forecastData: transformForecast(data) })) )
+  }
+}
+
+export const setWeather = payload => {
+  return dispatch => {
+    payload.forEach( city => {
+      dispatch(clearWeatherCity(city))
+      const api_weather = getUrlWeatherByCity('weather', city);
+      fetch(api_weather)
+      .then(res => res.json())
+      .then( data => {
+        const weatherData = transformWeather(data) 
+        dispatch(setWeatherCity({city, weatherData}))
+      })
+    })
   }
 }

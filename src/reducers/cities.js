7ce4f46fc33aa1f5f3374bebf3a8fad962149ac5
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-04 14:40:54 -0600

    :fire: setWeatherCity, clearWeatherCity reducers were created

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -1,11 +1,20 @@
-import { SET_FORECASTDATA } from './../actions'
+import { SET_FORECASTDATA, CLEAR_WEATHER_CITY, SET_WEATHER_CITY  } from './../actions'
 import { createSelector } from 'reselect'
 
 export const cities = (state = {}, action) => {
   switch (action.type) {
-    case SET_FORECASTDATA:
+    case SET_FORECASTDATA: {
       const { city, forecastData } = action.payload
-      return { ...state, [city]: { forecastData } }  
+      return { ...state, [city]: { forecastData } }
+    }
+    case CLEAR_WEATHER_CITY: {
+      const city = action.payload
+      return { ...state,  [city]: { weatherData: null } }
+    }
+    case SET_WEATHER_CITY: {
+      const { city, weatherData } = action.payload
+      return { ...state,  [city]: { weatherData } }
+    }
     default:
       return state
   }
@@ -14,4 +23,4 @@ export const cities = (state = {}, action) => {
 export const getForcastDataFromCities = createSelector(
   [(state, city) => cities[city] && cities[city].forecastData],
   forecastData => forecastData
-  )
+)
